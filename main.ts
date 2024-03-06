605f0bd23358873a1aae784d3a7cb57bf47e27d2
abdul kabeer khan <162288997+kabeer129@users.noreply.github.com>
2024-03-06 12:03:36 -0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,13 @@
+let vehicle = "bike"
+if (vehicle === 'car' ){
+ console.log("go to a car parking");
+}
+else if(vehicle === 'bike'){
+  console.log("go to a bike parking")
+}
+else if(vehicle === 'truck'){
+console.log("go to a truck parking");
+}
+else{
+console.log("no parking area for this vehicle");
+}
